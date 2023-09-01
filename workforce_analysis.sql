10224e1f3ef51e30fb0909ca1e2fe003af398a93
rahul-learning0727 <68843932+rahul-learning0727@users.noreply.github.com>
2023-09-01 15:07:48 +0530

    Update workforce_analysis.sql

--- workforce_analysis.sql
+++ workforce_analysis.sql
@@ -33,7 +33,7 @@ ProjectName varchar(200)
 );
 
 insert into ProjectDetail values
-					(1,1,'Task Track'),
+		    (1,1,'Task Track'),
                     (2,1,'CLP'),
                     (3,1,'Survey Managment'),
                     (4,2,'HR Managment'),
@@ -275,7 +275,7 @@ Create table Employees
      Salary int
 );
 Insert into Employees values (1,'Ben', 'Hoskins', 'Male', 70000),
-							 (2,'Mark', 'Hastings', 'Male', 60000),
+			     (2,'Mark', 'Hastings', 'Male', 60000),
                              (3,'Steve', 'Pound', 'Male', 45000),
                              (4,'Ben', 'Hoskins', 'Male', 70000),
                              (5,'Philip', 'Hastings', 'Male', 45000),
@@ -291,3 +291,182 @@ where N-1 = ( select count(distinct e2.salary) from employees as e2
 select ID, max(distinct salary) as sal from employees e1
 where 3-1 = ( select count(distinct e2.salary) from employees as e2
 			where e2.salary > e1.salary);
+
+-- 2) SQL QUERY TO GET ORGANIZATION HIERARCHY
+-- 3) DELET DUPLICATE ROWS IN SQL
+alter table Employees
+drop primary key;
+
+desc Employees;
+
+update Employees 
+set ID = 1
+where ID =4;
+
+with Employees_cte as (
+		select *, row_number() over (partition by FirstName order by FirstName)
+        as rownumber
+        from Employees
+        )
+select * from Employees_cte
+where rownumber >1;
+
+with Employees_cte as (
+		select *, row_number() over (partition by FirstName order by FirstName)
+        as rownumber
+        from Employees
+        )
+delete from Employees_cte           #  to delete use DELETE in place of select
+where rownumber >1;
+
+-- 4) FIND EMPLOYEES HIRED IN LAST N M0NTHS
+Create table Emp
+(
+     ID int primary key,
+     FirstName varchar(50),
+     LastName varchar(50),
+     Gender varchar(50),
+     Salary int,
+     HireDate varchar(20)
+);
+
+desc emp;
+update emp
+set HireDate = str_to_date(HireDate, "%m/%d/%Y");
+Insert into Emp values
+			    (1,'Mark','Hastings','Male',60000,'5/10/2023'),
+                            (2,'Steve','Pound','Male',45000,'4/20/2023'),
+                            (3,'Ben','Hoskins','Male',70000,'4/5/2023'),
+                            (4,'Philip','Hastings','Male',45000,'3/11/2023'),
+                            (5,'Mary','Lambeth','Female',30000,'3/10/2023'),
+                            (6,'Valarie','Vikings','Female',35000,'2/9/2023'),
+                            (7,'Able','Edward','Male',5000,'1/22/2023'),
+                            (8,'Emma','Nan','Female',5000,'1/14/2023'),
+                            (9,'Jd','Nosin','Male',6000,'1/10/2022'),
+                            (10,'Todd','Heir','Male',7000,'2/14/2022'),
+                            (11,'San','Hughes','Male',7000,'3/15/2022'),
+                            (12,'Nico','Night','Male',6500,'4/19/2022'),
+                            (13,'Martin','Jany','Male',5500,'5/23/2022'),
+                            (14,'Mathew','Mann','Male',4500,'6/23/2022'),
+                            (15,'Baker','Barn','Male',3500,'7/23/2022'),
+                            (16,'Mosin','Barn','Male',8500,'8/21/2022'),
+                            (17,'Rachel','Aril','Female',6500,'9/14/2022'),
+                            (18,'Pameela','Son','Female',4500,'10/14/2022'),
+                            (19,'Thomas','Cook','Male',3500,'11/14/2022'),
+                            (20,'Malik','Md','Male',6500,'12/14/2022'),
+                            (21,'Josh','Anderson','Male',4900,'5/1/2023'),
+                            (22,'Geek','Ging','Male',2600,'4/1/2023'),
+                            (23,'Sony','Sony','Male',2900,'4/30/2023'),
+                            (24,'Aziz','Sk','Male',3800,'3/1/2023'),
+                            (25,'Amit','Naru','Male',3100,'3/31/2023');
+                            
+update emp
+set HireDate = str_to_date(HireDate, "%m/%d/%Y");
+                            
+select * from emp;
+
+alter table emp
+modify HireDate date;
+
+select * from emp
+where timestampdiff(month,HireDate , utc_timestamp )between 1 and 3;
+
+-- 6) QUERY TO FIND ROWS THAT CONTAIN ONLY NUMERICAL DATA
+Create Table TestTable
+(
+     ID int primary key,
+     Value varchar(50)
+);
+
+Insert into TestTable values 
+			(1,'123'),
+			(2,'ABC'),
+			(3,'DEF'),
+			(4,'901'),
+			(5,'JKL');
+-- for numeric
+select * from TestTable                # here ^ - Begining of string
+where value regexp "^[0-9]+$";         # $ - End of string 
+				       # [..] - any charracter listed between sq brackets
+-- for strings
+select * from TestTable
+where value regexp "^[a-z]+$"; 
+
+-- 7) QUERY TO FIND DEPARTMENT WITH HIGHEST NUMBER OF EMPLOYEES 
+Create Table Departments
+(
+     DepartmentID int primary key,
+     DepartmentName varchar(50)
+);
+Insert into Departments values 
+			(1, 'IT'),
+			(2, 'HR'),
+			(3, 'Payroll');
+                                
+Create Table dept_Employees
+(
+     EmployeeID int primary key,
+     EmployeeName varchar(50),
+     DepartmentID int
+);
+Insert into dept_Employees values   
+				(1, 'Mark', 1),
+				(2, 'John', 1),
+				(3, 'Mike', 1),
+				(4, 'Mary', 2),
+				(5, 'Stacy', 3);
+                                    
+select * from Departments;
+select * from dept_Employees;
+
+select DepartmentName, count(D2.DepartmentID) as dept_count from Departments as D1 join dept_Employees as D2
+on D1.DepartmentID = D2.DepartmentID
+group by DepartmentName
+order by dept_count desc
+limit 1;
+
+-- 9) JOIN 3 TABLES IN SQL
+select * from dept_Employees;
+		-- insert values in columns field --
+insert into dept_Employees values (6,'Valarie',2);
+
+		-- add column in a table --
+alter table  dept_Employees
+add Genders int;
+
+		-- rename column name in a table --
+alter table  dept_Employees
+rename column Genders to GenderID;
+
+		-- create new table --
+create table Genders (
+	GenderID int,
+    Gender varchar(20)
+);
+
+insert into Genders values (1,'Male'),
+			   (2,'Female');
+select * from Genders;
+
+		-- add values in a column based on other column conditions --
+update dept_Employees
+set GenderID = case 
+		when DepartmentID = 1 then 1  # add values using single condition
+		when DepartmentID in (2, 3) then 2 # add values using multiple condition
+                end;
+                    
+		-- delete all values of a particular column --
+update dept_Employees
+set GenderID = null;
+
+select * from dept_Employees;
+select * from Genders;
+select * from Departments;
+
+
+		-- join 3 or more tables --
+select EmployeeID, EmployeeName, Gender, DepartmentName from dept_Employees as E
+join Departments as D
+on E.DepartmentID = D.DepartmentID
+join Genders as G
+on E.GenderID = G.GenderID;
